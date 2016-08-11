4591a333a816f42140a29b91ca7e973983a70439
mpahic <marko.pahic@asseco-see.hr>
2016-08-11 13:31:34 +0200

    added repository initializer for node definitions

--- src/main/java/org/cloudcog/gears/sdk/RepositoryInitializer.java
+++ src/main/java/org/cloudcog/gears/sdk/RepositoryInitializer.java
@@ -1,37 +1,37 @@
-//package org.cloudcog.gears.sdk;
-//
-//import java.io.InputStreamReader;
-//
-//import javax.servlet.ServletContextEvent;
-//import javax.servlet.ServletContextListener;
-//import javax.servlet.annotation.WebListener;
-//
-//import org.apache.jackrabbit.commons.cnd.CndImporter;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//@WebListener
-//public class RepositoryInitializer implements ServletContextListener {
-//
-//	private static final Logger log = LoggerFactory.getLogger(RepositoryInitializer.class);
-//
-//	@Override
-//	public void contextInitialized(ServletContextEvent sce) {
-//		try {
-//			CndImporter.registerNodeTypes(
-//					new InputStreamReader(
-//							this.getClass().getResourceAsStream("/org/cloudcog/gears/repository/data/nodeTypes.cnd")),
-//					RepositoryContext.getJcrSession());
-//		} catch (Exception e) {
-//			log.error(e.getMessage(), e);
-//		}
-//
-//	}
-//
-//	@Override
-//	public void contextDestroyed(ServletContextEvent sce) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//}
+package org.cloudcog.gears.sdk;
+
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.apache.jackrabbit.commons.cnd.CndImporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebListener
+public class RepositoryInitializer implements ServletContextListener {
+
+	private static final Logger log = LoggerFactory.getLogger(RepositoryInitializer.class);
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		try {
+			CndImporter.registerNodeTypes(
+					new InputStreamReader(
+							this.getClass().getResourceAsStream("/org/cloudcog/gears/repository/data/nodeTypes.cnd")),
+					RepositoryContext.getJcrSession());
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
